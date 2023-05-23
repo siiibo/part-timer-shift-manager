49bb9acf2f094c1afc7ced34d7c3c7f87ef62a7a
hi-se <40375981+hi-se@users.noreply.github.com>
2023-05-23 09:31:44 +0900

    feat(notify): Dailyでアルバイトシフトの通知をSlackに流す
    現状 #part-timer チャンネルで毎朝9時に流している「今日のシフト」情報は、Slackbotのリマインダー機能とGAS製Slack
    Appを利用して通知を流しているが、ここ最近Slack
    Appの3秒ルールを守れずに通知が重複している日が多いので、GASのタイムトリガーを用いた通知に切り替える
    
    🔗 https://trello.com/c/fWr8KHHN
    
    splitted from https://github.com/siiibo/hisyonosuke
    ref: https://github.com/siiibo/hisyonosuke/pull/34

---
+++ src/notify-daily-shift.ts
@@ -0,0 +1,71 @@
+import { GasWebClient as SlackClient } from "@hi-se/web-api";
+import { set } from "date-fns";
+
+import { getConfig } from "./config";
+
+const ANNOUNCE_HOUR = 9;
+
+export function initNotifyDailyShift() {
+  const targetFunction = notifyDailyShift;
+
+  ScriptApp.getProjectTriggers().forEach((trigger) => {
+    if (trigger.getHandlerFunction() === targetFunction.name) {
+      ScriptApp.deleteTrigger(trigger);
+    }
+  });
+
+  ScriptApp.newTrigger(targetFunction.name)
+    .timeBased()
+    .atHour(ANNOUNCE_HOUR - 1)
+    .everyDays(1)
+    .create();
+}
+
+export function notifyDailyShift() {
+  const { CALENDAR_ID, SLACK_CHANNEL_TO_POST } = getConfig();
+  const client = getSlackClient();
+  const calendar = CalendarApp.getCalendarById(CALENDAR_ID);
+
+  const now = new Date();
+  if (!checkTime(now)) throw new Error(`設定時刻に誤りがあります.\nANNOUNCE_HOUR: ${ANNOUNCE_HOUR}\nnow: ${now}`);
+
+  const targetDate = new Date();
+  const announceTime = set(targetDate, { hours: ANNOUNCE_HOUR, minutes: 0, seconds: 0, milliseconds: 0 });
+  const dailyShifts = calendar.getEventsForDay(targetDate);
+  const notificationString = getNotificationString(dailyShifts);
+
+  client.chat.scheduleMessage({
+    channel: SLACK_CHANNEL_TO_POST,
+    post_at: getUnixTimeStampString(announceTime),
+    text: notificationString,
+  });
+}
+
+function getNotificationString(events: GoogleAppsScript.Calendar.CalendarEvent[]): string {
+  return !events.length
+    ? "今日の予定はありません"
+    : events.map(getNotificationStringForEvent).join("\n") +
+        "\n\n" +
+        ":calendar: 勤務開始時に<https://calendar.google.com/calendar|カレンダー>に予定が入っていないか確認しましょう！";
+}
+
+function getNotificationStringForEvent(event: GoogleAppsScript.Calendar.CalendarEvent): string {
+  const title = event.getTitle();
+  const startTime = Utilities.formatDate(event.getStartTime(), "Asia/Tokyo", "HH:mm");
+  const endTime = Utilities.formatDate(event.getEndTime(), "Asia/Tokyo", "HH:mm");
+  return `${title}  ${startTime} 〜 ${endTime}`;
+}
+
+function checkTime(target: Date) {
+  return target.getHours() === ANNOUNCE_HOUR - 1;
+}
+
+function getUnixTimeStampString(date: Date): string {
+  return Math.floor(date.getTime() / 1000).toFixed();
+}
+
+function getSlackClient() {
+  const token = PropertiesService.getScriptProperties().getProperty("SLACK_TOKEN");
+  if (!token) throw new Error("SLACK_TOKEN is not set");
+  return new SlackClient(token);
+}

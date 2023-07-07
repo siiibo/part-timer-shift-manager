f6a288801c471ea9caa4c6999d744a4e8ba45b78
Luliko-hub <79243520+Luliko-hub@users.noreply.github.com>
2023-07-07 11:44:48 +0900

    feat: アルバイトのシフト変更用ツールの作成
    シフト変更の操作をスプレッドシート上で行うようにした。
    参考Trello (https://trello.com/c/9P3k440s)
    
    splitted from https://github.com/siiibo/hisyonosuke
    ref: https://github.com/siiibo/hisyonosuke/pull/

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+const ConfigSchema = z.object({
+  API_URL: z.string(),
+  CALENDAR_ID: z.string(),
+  SLACK_ACCESS_TOKEN: z.string(),
+  SLACK_CHANNEL_TO_POST: z.string(),
+  JOB_SHEET_URL: z.string(),
+  MEMBER_ID: z.string(),
+});
+
+export type Config = z.infer<typeof ConfigSchema>;
+
+let config: Config;
+
+export const getConfig = () => {
+  if (!config) {
+    const props = PropertiesService.getScriptProperties().getProperties();
+    config = ConfigSchema.parse(props);
+  }
+  return config;
+};
---
+++ src/shift-changer-api.ts
@@ -0,0 +1,116 @@
+import { addWeeks } from "date-fns";
+
+import { getConfig } from "./config";
+
+export type EventInfo = { title: string; date: string; startTime: string; endTime: string };
+
+const getCalendar = () => {
+  const { CALENDAR_ID } = getConfig();
+  const calendar = CalendarApp.getCalendarById(CALENDAR_ID);
+  return calendar;
+};
+
+const isEventGuest = (event: GoogleAppsScript.Calendar.CalendarEvent, email: string) => {
+  const guestEmails = event.getGuestList().map((guest) => guest.getEmail());
+  return guestEmails.includes(email);
+};
+
+export const shiftChanger = (e: GoogleAppsScript.Events.DoPost) => {
+  const operationType = e.parameter.operationType;
+  const userEmail = e.parameter.userEmail;
+  switch (operationType) {
+    case "registration": {
+      const registrationInfos = JSON.parse(e.parameter.registrationInfos);
+      registration(userEmail, registrationInfos);
+      break;
+    }
+    case "modificationAndDeletion": {
+      const modificationInfos = JSON.parse(e.parameter.modificationInfos);
+      const deletionInfos = JSON.parse(e.parameter.deletionInfos);
+      modification(modificationInfos, userEmail);
+      deletion(deletionInfos, userEmail);
+      break;
+    }
+    case "showEvents": {
+      const startDate = new Date(e.parameter.startDate);
+      const eventInfos = showEvents(userEmail, startDate);
+      return JSON.stringify(eventInfos);
+    }
+  }
+  return;
+};
+
+const registration = (userEmail: string, registrationInfos: EventInfo[]) => {
+  registrationInfos.forEach((registrationInfo) => {
+    registerEvent(registrationInfo, userEmail);
+  });
+};
+
+const registerEvent = (eventInfo: EventInfo, userEmail: string) => {
+  const calendar = getCalendar();
+  const [startDate, endDate] = getStartEndDate(eventInfo);
+  calendar.createEvent(eventInfo.title, startDate, endDate, { guests: userEmail });
+};
+
+const showEvents = (userEmail: string, startDate: Date): EventInfo[] => {
+  const endDate = addWeeks(startDate, 1);
+  const calendar = getCalendar();
+  const events = calendar.getEvents(startDate, endDate).filter((event) => isEventGuest(event, userEmail));
+  const eventInfos = events.map((event) => {
+    const title = event.getTitle();
+    const date = Utilities.formatDate(event.getStartTime(), "JST", "MM/dd");
+    const startTime = Utilities.formatDate(event.getStartTime(), "JST", "HH:mm");
+    const endTime = Utilities.formatDate(event.getEndTime(), "JST", "HH:mm");
+
+    return { title, date, startTime, endTime };
+  });
+  return eventInfos;
+};
+
+const modification = (
+  modificationInfos: {
+    previousEventInfo: EventInfo;
+    newEventInfo: EventInfo;
+  }[],
+  userEmail: string
+) => {
+  const calendar = getCalendar();
+  modificationInfos.forEach((eventInfo) => modifyEvent(eventInfo, calendar, userEmail));
+};
+
+const modifyEvent = (
+  eventInfo: {
+    previousEventInfo: EventInfo;
+    newEventInfo: EventInfo;
+  },
+  calendar: GoogleAppsScript.Calendar.Calendar,
+  userEmail: string
+) => {
+  const [startDate, endDate] = getStartEndDate(eventInfo.previousEventInfo);
+
+  const newTitle = eventInfo.newEventInfo.title;
+
+  const [newStartDate, newEndDate] = getStartEndDate(eventInfo.newEventInfo);
+
+  const event = calendar.getEvents(startDate, endDate).find((event) => isEventGuest(event, userEmail));
+  if (!event) return;
+  event.setTime(newStartDate, newEndDate);
+  event.setTitle(newTitle);
+};
+
+const getStartEndDate = ({ date, startTime, endTime }: EventInfo): [Date, Date] => {
+  const startDate = new Date(`${date} ${startTime}`);
+  const endDate = new Date(`${date} ${endTime}`);
+  return [startDate, endDate];
+};
+const deletion = (deletionInfos: EventInfo[], userEmail: string) => {
+  const calendar = getCalendar();
+  deletionInfos.forEach((eventInfo) => deleteEvent(eventInfo, calendar, userEmail));
+};
+
+const deleteEvent = (eventInfo: EventInfo, calendar: GoogleAppsScript.Calendar.Calendar, userEmail: string) => {
+  const [startDate, endDate] = getStartEndDate(eventInfo);
+  const event = calendar.getEvents(startDate, endDate).find((event) => isEventGuest(event, userEmail));
+  if (!event) return;
+  event.deleteEvent();
+};

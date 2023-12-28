809b74c3937c83bd9c4981f1135a5cf9ca565d9a
たくや <takuyadayo014@gmail.com>
2023-12-28 09:07:26 +0000

    refactor: 予定を表示する際に年情報を与えるように変更

--- src/shift-changer.ts
+++ src/shift-changer.ts
@@ -145,7 +145,7 @@ export const callShowEvents = () => {
   if (eventInfos.length === 0) throw new Error("no events");
 
   const moldedEventInfos = eventInfos.map(({ title, date, startTime, endTime }) => {
-    const dateStr = Utilities.formatDate(date, "JST", "MM/dd");
+    const dateStr = Utilities.formatDate(date, "JST", "yyyy/MM/dd");
     const startTimeStr = Utilities.formatDate(startTime, "JST", "HH:mm");
     const endTimeStr = Utilities.formatDate(endTime, "JST", "HH:mm");
     return [title, dateStr, startTimeStr, endTimeStr];

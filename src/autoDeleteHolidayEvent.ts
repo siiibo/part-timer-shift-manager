4a44a291a35f62dc977eade764112f1cc7c4401f
たくや <takuyadayo014@gmail.com>
2024-06-27 02:58:33 +0000

    feat: 年末年始も自動削除するようにした

--- src/autoDeleteHolidayEvent.ts
+++ src/autoDeleteHolidayEvent.ts
@@ -1,4 +1,4 @@
-import { addMonths } from "date-fns";
+import { addMonths, getDate, getMonth } from "date-fns";
 
 import { getConfig } from "./config";
 
@@ -18,11 +18,11 @@ export const deleteHolidayShift = () => {
   const { CALENDAR_ID } = getConfig();
   const calendar = CalendarApp.getCalendarById(CALENDAR_ID);
   const today = new Date();
-  const twoMonthLater = addMonths(today, 2);
+  const twoMonthLater = addMonths(today, 10);
   const events = calendar.getEvents(today, twoMonthLater);
   events.forEach((event) => {
     const statTime = new Date(event.getStartTime().getTime());
-    if (isHoliday(statTime)) {
+    if (isHolidayOrSpecialDate(statTime)) {
       event.deleteEvent();
     }
   });
@@ -33,4 +33,21 @@ const isHoliday = (day: Date): boolean => {
   const calendar = CalendarApp.getCalendarById(calendarId);
   const holidayEvents = calendar.getEventsForDay(day);
   return holidayEvents.length > 0;
+};
+
+const isHolidayOrSpecialDate = (date: Date): boolean => {
+  // 12/31
+  if (getMonth(date) === 11 && getDate(date) === 31) {
+    return true;
+  }
+  // 1/1 ~ 1/3
+  if (getMonth(date) === 0) {
+    if (getDate(date) === 1 || getDate(date) === 2 || getDate(date) === 3) {
+      return true;
+    }
+  }
+  if (isHoliday(date)) {
+    return true;
+  }
+  return false;
 };

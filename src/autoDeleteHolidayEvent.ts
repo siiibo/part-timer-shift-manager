e28f20c5d387efa85d23ea22a7ba329b66d38583
たくや <takuyadayo014@gmail.com>
2024-07-04 03:44:54 +0000

    refactor: 2というファイルスコープでの定数定義を行った

--- src/autoDeleteHolidayEvent.ts
+++ src/autoDeleteHolidayEvent.ts
@@ -3,13 +3,15 @@ import { addWeeks } from "date-fns";
 import { getConfig } from "./config";
 import { isBankHoliday } from "./date-utils";
 
+const DELETE_HOLIDAY_SHIFT_INTERVAL = 2;
+
 export const initDeleteHolidayShift = () => {
   ScriptApp.getProjectTriggers()
     .filter((trigger) => trigger.getHandlerFunction() === deleteHolidayShift.name)
     .forEach((trigger) => ScriptApp.deleteTrigger(trigger));
   ScriptApp.newTrigger(deleteHolidayShift.name)
     .timeBased()
-    .everyWeeks(2)
+    .everyWeeks(DELETE_HOLIDAY_SHIFT_INTERVAL)
     .onWeekDay(ScriptApp.WeekDay.SUNDAY)
     .atHour(0)
     .create();
@@ -19,7 +21,7 @@ export const deleteHolidayShift = () => {
   const { CALENDAR_ID } = getConfig();
   const calendar = CalendarApp.getCalendarById(CALENDAR_ID);
   const today = new Date();
-  const twoWeekLater = addWeeks(today, 2);
+  const twoWeekLater = addWeeks(today, DELETE_HOLIDAY_SHIFT_INTERVAL);
   const events = calendar.getEvents(today, twoWeekLater);
   events.forEach((event) => {
     const statTime = new Date(event.getStartTime().getTime());
